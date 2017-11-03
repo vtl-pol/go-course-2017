2330322a524eed764237ba970352d7898980e406
Veets <32930550+rndlinker37@users.noreply.github.com>
2017-11-03 18:29:51 +0200

    edit

--- homeworks/veets.seer_rndlinker37/homework_3/mario.go
+++ homeworks/veets.seer_rndlinker37/homework_3/mario.go
@@ -7,9 +7,8 @@ func main() {
 
 	mario := 4 // default value
 	X := "#"   // default char to print
-	//	var mariomin int = 1		// ! it will be inferred from the right-hand side
-	//	var mariomax int = 24		// ! it will be inferred from the right-hand side
-	mariomin := 1
+
+	mariomin := 2
 	mariomax := 24
 
 	fmt.Print("Enter Mario number: ")
@@ -25,47 +24,27 @@ func main() {
 	}
 
 	fmt.Println("")
-	// print n-space for "Height" align center
+
 	for n := 1; n <= mario-3; n++ {
 		fmt.Print(" ")
 	}
 	fmt.Println("Height: ", mario)
-
-	// print "input" lines
 	for i := 0; i <= mario; i++ {
-
 		fmt.Print(" ")
-		// print n-space
 		for n := 1; n <= mario-i; n++ {
 			fmt.Print(" ")
 		}
-		// print n-symbol
 		for n := 1; n <= i; n++ {
 			fmt.Print(X)
 		}
-
-		// print 2 space
 		fmt.Print("  ")
-
-		// print n-symbol
 		for n := 1; n <= i; n++ {
 			fmt.Print(X)
 		}
-		// print n-space
 		for n := 1; n <= mario-i; n++ {
 			fmt.Print(" ")
 		}
-		// end line & LF
 		fmt.Println(" ")
 	}
-
-	//	fmt.Println("Height:  4")
-	//	fmt.Println("   *__*   ")
-	//	fmt.Println("  **__**  ")
-	//	fmt.Println(" ***__*** ")
-	//	fmt.Println("****__****")
-
-	fmt.Println("")
-	fmt.Println("Hello veets.seer-rndlinker37")
 
 }

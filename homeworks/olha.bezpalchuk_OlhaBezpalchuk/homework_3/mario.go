eb9474a721a62d9eac6913dcfcf90d5343cb7fc4
Olha Bezpalchuk <olha.bezpalchuk@gmail.com>
2017-11-05 22:38:46 +0200

    changed definitions of functions and variables

--- homeworks/olha.bezpalchuk_OlhaBezpalchuk/homework_3/mario.go
+++ homeworks/olha.bezpalchuk_OlhaBezpalchuk/homework_3/mario.go
@@ -4,37 +4,32 @@ import (
 	"fmt"
 )
 
-func build_line(material string, brick_length int, line_length int) (line string) {
-	line = ""
-
-	piece := ""
-	spaces := ""
-
+func BuildLine(material string, brickLength int, lineLength int) string {
+	var piece, spaces string
+	
 	// use bricks
-	for i := 0; i < brick_length; i++ {
+	for i := 0; i < brickLength; i++ {
 		piece += material
 	}
 
 	// add spaces to the line
-	for i := brick_length; i < line_length; i++ {
+	for i := brickLength; i < lineLength; i++ {
 		spaces += " "
 	}
-
-	line = spaces + piece + "  " + piece + spaces 
-	return
+ 
+	return spaces + piece + "  " + piece + spaces
 }
 
-func build_pyramid(material string, height int) () {
+func BuildPyramid(material string, height int) {
 	fmt.Printf("Height: %d\n", height)
 	for i := 1; i <= height; i++ {
-		fmt.Println(build_line(material, i, height))
+		fmt.Println(BuildLine(material, i, height))
 	}
 }
 
 func main() {
-
 	brick := "#"
 	height := 5
 
-	build_pyramid(brick, height)
+	BuildPyramid(brick, height)
 }
